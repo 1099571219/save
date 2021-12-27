370435d7169a8a2830a494e287e9d7281f4bd4d2
lennon <1099571219@qq.com>
2021-12-27 21:25:35 +0800

    添加 <Nav/> 样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
+  <nav>
+    <router-link to="/money" class="item">
       <Icon name="money" />
       记账
     </router-link>
-    |
-    <router-link to="/labels">
+    <router-link to="/labels" class="item">
       <Icon name="label1" />
       标签
     </router-link>
-    |
-    <router-link to="/statistics">
+    <router-link to="/statistics" class="item">
       <Icon name="statistics" />
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -25,7 +23,21 @@ export default Vue.extend({
 </script>
 
 <style lang='scss' scoped>
-.nav {
-  border: 1px solid red;
+nav {
+  display: flex;
+  box-shadow: 0 0 5px rgba($color: #000000, $alpha: 0.3);
+
+  > .item {
+    padding: 2px 0;
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    justify-content: center;
+    align-items: center;
+    .icon {
+      width: 30px;
+      height: 30px;
+    }
+  }
 }
 </style>
